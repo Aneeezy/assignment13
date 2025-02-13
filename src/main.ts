833e5ed6265bc6c8ddba07e2188b6a4b82b6f37c
Aneeezy <anissanyamhandu@gmail.com>
2025-02-13 11:23:19 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -104,7 +104,6 @@ function restartGame() {
 function saveGameState() {
     localStorage.setItem("boardState", JSON.stringify(options));
     localStorage.setItem("currentPlayer", currentPlayer);
-    localStorage.setItem("statusText", statusText.textContent || "");
 }
 
 
